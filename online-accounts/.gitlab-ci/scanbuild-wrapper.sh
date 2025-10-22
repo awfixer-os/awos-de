b0322a9d3afd4e4523c2d985f70b9fd0734b2857
awfixer <git@awfixer.me>
2025-10-21 20:34:29 -0700

    FUCK GITLAB_CI

--- online-accounts/.gitlab-ci/scanbuild-wrapper.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -e
-
-# deadcode.DeadStores: allows assigning a value to an auto-cleanup
-# plist-html: see `scanbuild-plist-to-junit.py`
-scan-build --status-bugs \
-           -disable-checker deadcode.DeadStores \
-           -plist-html \
-           -v \
-           "$@"
